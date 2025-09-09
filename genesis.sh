d268b92c234af6bd2f9a25356bdde3542de9ddf9
Steve <contact@stevedylan.dev>
2025-09-09 09:08:21 -0400

    chore: changed loading state

--- genesis.sh
+++ genesis.sh
@@ -14,15 +14,15 @@ Where building on Ethereum begins
 "
 
 # Install foundryup
-echo "Installing Foundry..."
+printf "Installing Foundry..."
 curl -fsS https://foundry.paradigm.xyz | bash > /dev/null 2>&1
-echo "✔️ Foundry installed"
+printf "\r✔️ Foundry installed    \n"
 
 # Install foundry
 foundryup > /dev/null 2>&1
 
 # Install Hardhat
-echo "Installing Hardhat..."
+printf "Installing Hardhat..."
 if command -v pnpm >/dev/null 2>&1; then
     pnpm install -g hardhat > /dev/null 2>&1
 elif command -v npm >/dev/null 2>&1; then
@@ -30,16 +30,16 @@ elif command -v npm >/dev/null 2>&1; then
 elif command -v bun >/dev/null 2>&1; then
     bun install -g hardhat > /dev/null 2>&1
 else
-    echo "No package manager found (pnpm, npm, or bun)"
+    printf "\rNo package manager found (pnpm, npm, or bun)\n"
+    exit 1
 fi
-echo "✔️ HardHat installed"
+printf "\r✔️ HardHat installed    \n"
 
 
 # Create wallet
 echo "❯ What do you want to name your wallet?"
 read wallet_name
 cast wallet new --password ~/.foundry/keystores $wallet_name
-echo "✔️ Wallet created"
 
 echo "
 ✔️ Installation complete!
